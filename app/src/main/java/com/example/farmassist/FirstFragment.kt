38a046ecfeb9e97727b95c6614a98068b29a01c0
Serene Babu <59754107+serenebkj2code@users.noreply.github.com>
2020-11-07 10:46:40 +0530

    SplashScreen Modified

--- app/src/main/java/com/example/farmassist/FirstFragment.kt
+++ app/src/main/java/com/example/farmassist/FirstFragment.kt
@@ -7,8 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
-
-
 class FirstFragment : Fragment() {
 
     override fun onCreateView(
