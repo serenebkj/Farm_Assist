e33b59ec6cccd0210582771e78ab6ae1fb882986
Serene Babu <59754107+serenebkj2code@users.noreply.github.com>
2020-12-11 12:22:28 +0530

    Plants videoview added

--- app/src/main/java/com/example/farmassist/PlantActivity.kt
+++ app/src/main/java/com/example/farmassist/PlantActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_plant.*
-
+import android.net.Uri;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 class PlantActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_plant)
+
         buttonOn.setOnClickListener {
             val database = FirebaseDatabase.getInstance()
             val myRef = database.getReference().child("FarmAssist").child("motor")
@@ -23,5 +26,7 @@ class PlantActivity : AppCompatActivity() {
 
             myRef.setValue("0")
         }
+        var vidPlant:VideoView=findViewById(R.id.plantVideo)
+        
     }
 }
